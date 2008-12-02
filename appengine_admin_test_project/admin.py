cb984cda9fb1729911d6afdf0417e4c23770be54
valdiic <valdiic@01ea29de-ada2-11dd-8075-91d349ce83ee>
2008-12-02 23:15:06 +0000

    Test project import

---
+++ appengine_admin_test_project/admin.py
@@ -0,0 +1,55 @@
+﻿import models
+import appengine_admin
+
+## Admin views ##
+class AdminTestModel(appengine_admin.ModelAdmin):
+    model = models.TestModel
+    'stringField', 'textField', 'choicesField', 'integerField', 'booleanField', 'referenceField', 'whencreated', 'whenupdated'
+    listFields = (
+        'stringField',
+        'textField',
+        'choicesField',
+        'integerField',
+        'booleanField',
+        'referenceField',
+        'dateField',
+        'timeField',
+        'userField',
+        'categoryField',
+        'linkField',
+        'emailField',
+        'imField',
+        'phoneNumberField',
+        'postalAddressField',
+        'ratingField',
+        'whencreated',
+        'whenupdated')
+    editFields = (
+        'stringField',
+        'textField',
+        'choicesField',
+        'integerField',
+        'booleanField',
+        'referenceField',
+        'dateField',
+        'timeField',
+        'userField',
+        'categoryField',
+        'linkField',
+        'emailField',
+        'imField',
+        'phoneNumberField',
+        'postalAddressField',
+        'ratingField')
+    readonlyFields = ('whencreated', 'whenupdated')
+    listGql = 'order by stringField, integerField'
+
+class AdminReferencedModel(appengine_admin.ModelAdmin):
+    model = models.ReferencedModel
+    listFields = ('name', 'whencreated', 'whenupdated')
+    editFields = ('name',)
+    readonlyFields = ('whencreated', 'whenupdated')
+    listGql = 'order by name'
+    
+appengine_admin.register(AdminTestModel, AdminReferencedModel)
+
---
+++ appengine_admin_test_project/gae_devserver.bat
@@ -0,0 +1,2 @@
+@echo off
+dev_appserver.py .\
