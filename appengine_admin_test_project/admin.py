dfad8b4f9ccd69f2be6320f7cf0d29812ac7702e
valdiic <valdiic@01ea29de-ada2-11dd-8075-91d349ce83ee>
2008-12-17 09:44:37 +0000

    Added test models for Referenced model that returns UTF-8 encoded string in __str__ method.

--- appengine_admin_test_project/admin.py
+++ appengine_admin_test_project/admin.py
@@ -1,10 +1,10 @@
 ﻿import models
+import models_utf8
 import appengine_admin
 
 ## Admin views ##
 class AdminTestModel(appengine_admin.ModelAdmin):
     model = models.TestModel
-    'stringField', 'textField', 'choicesField', 'integerField', 'booleanField', 'referenceField', 'whencreated', 'whenupdated'
     listFields = (
         'stringField',
         'textField',
@@ -50,6 +50,38 @@ class AdminReferencedModel(appengine_admin.ModelAdmin):
     editFields = ('name',)
     readonlyFields = ('whencreated', 'whenupdated')
     listGql = 'order by name'
+
+class AdminUtf8TestModel(appengine_admin.ModelAdmin):
+    model = models_utf8.Utf8TestModel
+    listFields = (
+        'stringField',
+        'textField',
+        'integerField',
+        'booleanField',
+        'referenceField',
+        'whencreated',
+        'whenupdated',
+    )
+    editFields = (
+        'stringField',
+        'textField',
+        'integerField',
+        'booleanField',
+        'referenceField',
+    )
+    readonlyFields = ('whencreated', 'whenupdated')
     
-appengine_admin.register(AdminTestModel, AdminReferencedModel)
+class AdminUtf8ReferencedModel(appengine_admin.ModelAdmin):
+    model = models_utf8.Utf8ReferencedModel
+    listFields = ('name', 'whencreated', 'whenupdated')
+    editFields = ('name',)
+    readonlyFields = ('whencreated', 'whenupdated')
+    listGql = 'order by name'
+    
+appengine_admin.register(
+    AdminTestModel,
+    AdminReferencedModel,
+    AdminUtf8TestModel,
+    AdminUtf8ReferencedModel,
+)
 
---
+++ appengine_admin_test_project/models_utf8.py
@@ -0,0 +1,23 @@
+﻿# -*- coding: utf-8 -*-
+from google.appengine.ext import db
+from appengine_admin.encoding import encoded_str_utf8
+
+# Models
+class Utf8ReferencedModel(db.Model):
+    name = db.StringProperty("Name", required = True)
+    whencreated = db.DateTimeProperty("Created", auto_now_add = True)
+    whenupdated = db.DateTimeProperty("Updated", auto_now = True)
+    
+    @encoded_str_utf8
+    def __str__(self):
+        return u"Glāžšķūņu rūķīši %s" % self.name
+
+class Utf8TestModel(db.Model):
+    stringField = db.StringProperty("String field")
+    textField = db.TextProperty("Text field")
+    integerField = db.IntegerProperty("Integer field", default = 0)
+    booleanField = db.BooleanProperty("Boolean field")
+    referenceField = db.ReferenceProperty(Utf8ReferencedModel, verbose_name = "Foreign key field")
+    whencreated = db.DateTimeProperty("Created", auto_now_add = True)
+    whenupdated = db.DateTimeProperty("Updated", auto_now = True)
+    
