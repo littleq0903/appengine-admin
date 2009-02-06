4cad4dc37bfcd9c8441f497ee76fee8c3c0afff0
valdiic <valdiic@01ea29de-ada2-11dd-8075-91d349ce83ee>
2009-02-06 14:56:31 +0000

    Copied test project to branch

---
+++ branches/appengine_admin_forms/appengine_admin_test_project/admin.py
@@ -0,0 +1,107 @@
+﻿import models
+import models_utf8
+import appengine_admin
+
+## Admin views ##
+class AdminTestModel(appengine_admin.ModelAdmin):
+    model = models.TestModel
+    listFields = (
+        'stringField',
+        'textField',
+        'choicesField',
+        'integerField',
+        'booleanField',
+        'referenceField',
+        'dateField',
+        'timeField',
+        'userField',
+        'categoryField',
+        'linkField',
+        'emailField',
+        'imField',
+        'phoneNumberField',
+        'postalAddressField',
+        'ratingField',
+        'whencreated',
+        'whenupdated')
+    editFields = (
+        'stringField',
+        'textField',
+        'choicesField',
+        'integerField',
+        'booleanField',
+        'referenceField',
+        'dateField',
+        'timeField',
+        'userField',
+        'categoryField',
+        'linkField',
+        'emailField',
+        'imField',
+        'phoneNumberField',
+        'postalAddressField',
+        'ratingField')
+    readonlyFields = ('whencreated', 'whenupdated')
+    listGql = 'order by stringField, integerField'
+
+class AdminReferencedModel(appengine_admin.ModelAdmin):
+    model = models.ReferencedModel
+    listFields = ('name', 'whencreated', 'whenupdated')
+    editFields = ('name',)
+    readonlyFields = ('whencreated', 'whenupdated')
+    listGql = 'order by name'
+
+class AdminUtf8TestModel(appengine_admin.ModelAdmin):
+    model = models_utf8.Utf8TestModel
+    listFields = (
+        'stringField',
+        'testMethod',
+        'textField',
+        'integerField',
+        'booleanField',
+        'referenceField',
+        'whencreated',
+        'whenupdated',
+    )
+    editFields = (
+        'stringField',
+        'textField',
+        'integerField',
+        'booleanField',
+        'referenceField',
+    )
+    readonlyFields = ('whencreated', 'whenupdated')
+
+class AdminUtf8ReferencedModel(appengine_admin.ModelAdmin):
+    model = models_utf8.Utf8ReferencedModel
+    listFields = ('name', 'whencreated', 'whenupdated')
+    editFields = ('name',)
+    readonlyFields = ('whencreated', 'whenupdated')
+    listGql = 'order by name'
+
+class AdminDerivedTestModel(appengine_admin.ModelAdmin):
+    model = models.DerivedTestModel
+    listFields = ('stringField',)
+    editFields = ('stringField', 'textField')
+
+class AdminDerivedTestModel2(appengine_admin.ModelAdmin):
+    model = models.DerivedTestModel2
+    listFields = ('blobField', 'stringField', 'booleanField', 'blobField')
+    editFields = ('stringField', 'textField', 'booleanField', 'blobField')
+    readonlyFields = ('blobField',)
+
+class AdminDerivedTestModel3(appengine_admin.ModelAdmin):
+    model = models.DerivedTestModel3
+    listFields = ('stringField', 'booleanField', 'whencreated', 'whenupdated')
+    editFields = ('stringField', 'textField', 'booleanField')
+    readonlyFields = ('whencreated', 'whenupdated')
+
+appengine_admin.register(
+    AdminTestModel,
+    AdminReferencedModel,
+    AdminUtf8TestModel,
+    AdminUtf8ReferencedModel,
+    AdminDerivedTestModel,
+    AdminDerivedTestModel2,
+    AdminDerivedTestModel3,
+)
---
+++ branches/appengine_admin_forms/appengine_admin_test_project/models.py
@@ -0,0 +1,58 @@
+﻿from google.appengine.ext import db
+
+
+choices = ['one', 'two', 'three', 'four']
+
+# Models
+class ReferencedModel(db.Model):
+    name = db.StringProperty("Name", required = True)
+    whencreated = db.DateTimeProperty("Created", auto_now_add = True)
+    whenupdated = db.DateTimeProperty("Updated", auto_now = True)
+    
+    def __unicode__(self):
+        return unicode(self.name)
+        
+    def __str__(self):
+        return self.__unicode__()
+
+class TestModel(db.Model):
+    stringField = db.StringProperty("String field")
+    textField = db.TextProperty("Text field")
+    choicesField = db.StringProperty("Choices field", choices = choices)
+    integerField = db.IntegerProperty("Integer field", default = 0)
+    booleanField = db.BooleanProperty("Boolean field")
+    referenceField = db.ReferenceProperty(ReferencedModel, verbose_name = "Foreign key field")
+    dateField = db.DateProperty("Date field")
+    timeField = db.TimeProperty("Time field")
+    userField = db.UserProperty("User field")
+    categoryField = db.CategoryProperty("Category field")
+    linkField = db.LinkProperty("Link field")
+    emailField = db.EmailProperty("Email field")
+    imField = db.IMProperty("IM Field")
+    phoneNumberField = db.PhoneNumberProperty("Phone number field")
+    postalAddressField = db.PostalAddressProperty("Postal address field")
+    ratingField = db.RatingProperty("Rating field")
+    whencreated = db.DateTimeProperty("Created", auto_now_add = True)
+    whenupdated = db.DateTimeProperty("Updated", auto_now = True)
+    
+    def __unicode__(self):
+        return self.stringField
+
+
+class BaseTestModel(db.Model):
+    stringField = db.StringProperty("String field in base class")
+    
+class DerivedTestModel(BaseTestModel):
+    textField = db.TextProperty("Text field in derived class")
+    
+class DerivedTestModel2(DerivedTestModel):
+    booleanField = db.BooleanProperty("Boolean field in derived class")
+    blobField = db.BlobProperty()
+    blobField_meta = db.BlobProperty()
+    
+class BaseTestModel2(db.Model):
+    whencreated = db.DateTimeProperty("Created", auto_now_add = True)
+    whenupdated = db.DateTimeProperty("Updated", auto_now = True)
+    
+class DerivedTestModel3(BaseTestModel2, DerivedTestModel2):
+    pass
