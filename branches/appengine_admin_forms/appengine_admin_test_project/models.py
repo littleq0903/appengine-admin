d4eea6e4db90b1a2a1d0ae5cf6ee145f2e05e4b6
valdiic <valdiic@01ea29de-ada2-11dd-8075-91d349ce83ee>
2009-02-15 22:28:56 +0000

    Updates for issue #13 (Use django newforms) * AdminForm now is built with custom widgets * Created widget adjustments for Django newforms usage * Copied Date, Time and SplitDatetTime widgets from Django 1.0 admin * Minor code cleanup * Added test code for editable DateTimeProperty (uses AdminSplitDateTime widget in UI)

--- branches/appengine_admin_forms/appengine_admin_test_project/models.py
+++ branches/appengine_admin_forms/appengine_admin_test_project/models.py
@@ -8,10 +8,10 @@ class ReferencedModel(db.Model):
     name = db.StringProperty("Name", required = True)
     whencreated = db.DateTimeProperty("Created", auto_now_add = True)
     whenupdated = db.DateTimeProperty("Updated", auto_now = True)
-    
+
     def __unicode__(self):
         return unicode(self.name)
-        
+
     def __str__(self):
         return self.__unicode__()
 
@@ -24,6 +24,7 @@ class TestModel(db.Model):
     referenceField = db.ReferenceProperty(ReferencedModel, verbose_name = "Foreign key field")
     dateField = db.DateProperty("Date field")
     timeField = db.TimeProperty("Time field")
+    datetimeField = db.DateTimeProperty("Date and Time field")
     userField = db.UserProperty("User field")
     categoryField = db.CategoryProperty("Category field")
     linkField = db.LinkProperty("Link field")
@@ -34,25 +35,25 @@ class TestModel(db.Model):
     ratingField = db.RatingProperty("Rating field")
     whencreated = db.DateTimeProperty("Created", auto_now_add = True)
     whenupdated = db.DateTimeProperty("Updated", auto_now = True)
-    
+
     def __unicode__(self):
         return self.stringField
 
 
 class BaseTestModel(db.Model):
     stringField = db.StringProperty("String field in base class")
-    
+
 class DerivedTestModel(BaseTestModel):
     textField = db.TextProperty("Text field in derived class")
-    
+
 class DerivedTestModel2(DerivedTestModel):
     booleanField = db.BooleanProperty("Boolean field in derived class")
     blobField = db.BlobProperty()
     blobField_meta = db.BlobProperty()
-    
+
 class BaseTestModel2(db.Model):
     whencreated = db.DateTimeProperty("Created", auto_now_add = True)
     whenupdated = db.DateTimeProperty("Updated", auto_now = True)
-    
+
 class DerivedTestModel3(BaseTestModel2, DerivedTestModel2):
     pass
