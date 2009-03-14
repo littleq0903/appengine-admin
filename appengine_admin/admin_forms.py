f4d98690fca5c74424e1b10607e37b3a91587f03
valdiic <valdiic@01ea29de-ada2-11dd-8075-91d349ce83ee>
2009-03-14 16:21:12 +0000

    * Some more support for BlobProperty file uploads. Now it saves the uploaded file properly. * Renamed old_get_value_for_form to original_get_value_for_form

--- appengine_admin/admin_forms.py
+++ appengine_admin/admin_forms.py
@@ -12,6 +12,7 @@ class AdminModelForm(djangoforms.ModelForm):
     """This class extends ModelForm to be able to pass additional attributes
         to the form while processing the request.
     """
+    enctype = ''
     def __init__(self, urlPrefix = '', *args, **kwargs):
         super(AdminModelForm, self).__init__(*args, **kwargs)
         self.urlPrefix = urlPrefix
@@ -52,6 +53,9 @@ def createAdminForm(formModel, editFields, editProps):
             # Choices must be set after creating the widget because in our case choices
             # is not a list but a wrapeper around query that always fetches fresh data from datastore
             field.widget.choices = field.choices
+        if getattr(field.widget, 'needs_multipart_form', False):
+            AdminForm.enctype = 'multipart/form-data'
+
     # Adjust widgets by property type
     for prop in editProps:
         if prop.typeName == 'DateProperty':
@@ -83,41 +87,44 @@ class FileField(forms.fields.Field):
 
     def clean(self, data, initial=None):
         super(FileField, self).clean(initial or data)
+
         if not self.required and data in forms.fields.EMPTY_VALUES:
             return None
-        elif not data and initial:
+        elif not data.value and initial:
             return initial
 
         # UploadedFile objects should have name and size attributes.
         try:
-            file_name = data.name
-            file_size = data.size
+            self.file_name = data.filename
+            self.file_size = len(data.value)
+            self.file_type = data.type
+            file_content = data.value
         except AttributeError:
             raise ValidationError(self.error_messages['invalid'])
 
-        if not file_name:
+        if not self.file_name:
             raise ValidationError(self.error_messages['invalid'])
-        if not file_size:
+        if not self.file_size:
             raise ValidationError(self.error_messages['empty'])
 
-        return data
+        return file_content
 forms.fields.FileField = FileField
 forms.FileField = FileField
 
 ### HACK HACK HACK ###
 # djangoforms.ReferenceProperty.get_value_for_form() does not catch the error that occurs
-# when referenved item is deleted.
+# when referenced item is deleted.
 # This "monkey patch" fixes the problem.
 def wrapped_get_value_for_form(self, instance):
     """Catch "ReferenceProperty failed to be resolved" error and return None.
     """
     try:
-        return old_get_value_for_form(self, instance)
+        return original_get_value_for_form(self, instance)
     except datastore_errors.Error, exc:
         # Error is raised if referenced property is deleted
         # Catch the exception and set value to none
         logging.warning('Error catched while getting item values: %s' % exc)
         return  None
 
-old_get_value_for_form = djangoforms.ReferenceProperty.get_value_for_form
+original_get_value_for_form = djangoforms.ReferenceProperty.get_value_for_form
 djangoforms.ReferenceProperty.get_value_for_form = wrapped_get_value_for_form
