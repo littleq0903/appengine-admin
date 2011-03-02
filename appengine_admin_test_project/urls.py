6ef5987e871ad97913700999ad3b2cbfcdc4e41a
valdiic <valdiic@01ea29de-ada2-11dd-8075-91d349ce83ee>
2011-03-02 09:35:13 +0000

    Let's use Django 1.2 by default

--- appengine_admin_test_project/urls.py
+++ appengine_admin_test_project/urls.py
@@ -1,6 +1,12 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+from google.appengine.dist import use_library
+use_library('django', '1.2')
+
 import admin
 import appengine_admin
 
